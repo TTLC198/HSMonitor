05f7ea440cac042c0263025ded627139ba3d9578
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-01-24 20:07:22 +0300

    Added type distribution.

---
+++ HSMonitor/Utils/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HSMonitor.Utils;
+
+public static class StringExtensions
+{
+    // https://stackoverflow.com/questions/4488969/split-a-string-by-capital-letters
+    public static IEnumerable<string> SplitByCapitalLettersConvention(this string source)
+    {
+        return Regex.Split(source, @"(?<!^)(?=[A-Z])|(?=[0-9])");
+    }
+}
