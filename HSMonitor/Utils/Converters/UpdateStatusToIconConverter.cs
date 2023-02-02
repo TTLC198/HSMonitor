66ca8ef484c12e48c34f4e3d8f9b84d1de90c1f4
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-02-02 21:59:57 +0300

    Added display of various status icons for the current update.

---
+++ HSMonitor/Utils/Converters/UpdateStatusToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using HSMonitor.ViewModels.Settings;
+using MaterialDesignThemes.Wpf;
+using NetSparkleUpdater.Enums;
+
+namespace HSMonitor.Utils.Converters;
+
+[ValueConversion(typeof(UpdateStatus), typeof(PackIconKind))]
+public class UpdateStatusToIconConverter : IValueConverter
+{
+    public static UpdateStatusToIconConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value switch
+    {
+#pragma warning disable CA1416
+        UpdateStatus.UpdateAvailable => PackIconKind.CloudDownloadOutline,
+        UpdateStatus.UpdateNotAvailable => PackIconKind.CloudCheckVariantOutline,
+        UpdateStatus.UserSkipped => PackIconKind.CloudCancelOutline,
+        _ => PackIconKind.CloudAlertOutline
+#pragma warning restore CA1416
+    };
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}
---
+++ HSMonitor/Utils/Converters/VisibilityBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HSMonitor.Utils.Converters;
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public class VisibilityBoolConverter : IValueConverter
+{
+    public static VisibilityBoolConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is true ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is Visibility.Visible;
+}
