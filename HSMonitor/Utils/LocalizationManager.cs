6c131680235ef56c6982420689ac18c9cf576174
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-07-03 18:23:31 +0300

    Added LocalizationManager

---
+++ HSMonitor/Utils/LocalizationManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+using HSMonitor.Properties;
+
+namespace HSMonitor.Utils;
+
+public static class LocalizationManager
+{
+    public static IEnumerable<CultureInfo> GetAvailableCultures()
+    {
+        var result = new List<CultureInfo>();
+
+        var rm = new ResourceManager(typeof(Resources));
+
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        foreach (var culture in cultures)
+        {
+            try
+            {
+                if (culture.Equals(CultureInfo.InvariantCulture)) continue; //do not use "==", won't work
+
+                var rs = rm.GetResourceSet(culture, true, false);
+                if (rs != null)
+                    result.Add(culture);
+            }
+            catch (CultureNotFoundException)
+            {
+                //NOP
+            }
+        }
+        return result;
+    }
+
+    public static void ChangeCurrentCulture(CultureInfo cultureInfo)
+    {
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+        Thread.CurrentThread.CurrentUICulture = cultureInfo;
+    }
+}
