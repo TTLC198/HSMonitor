cb0a7778ca49fe3376adf572c5e8b5607d606280
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-07-03 12:44:19 +0300

    Updated localization strings

--- HSMonitor/Properties/Resources.Designer.cs
+++ HSMonitor/Properties/Resources.Designer.cs
@@ -60,20 +60,263 @@ namespace HSMonitor.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to en-US.
+        ///   Looks up a localized string similar to About.
         /// </summary>
-        internal static string Resources {
+        internal static string AboutButtonContent {
             get {
-                return ResourceManager.GetString("Resources", resourceCulture);
+                return ResourceManager.GetString("AboutButtonContent", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ru-RU.
+        ///   Looks up a localized string similar to Autodetect Hardware:.
         /// </summary>
-        internal static string Resources_ru_RU {
+        internal static string AutoDetectHardwareSwitchText {
             get {
-                return ResourceManager.GetString("Resources.ru-RU", resourceCulture);
+                return ResourceManager.GetString("AutoDetectHardwareSwitchText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Start with Windows.
+        /// </summary>
+        internal static string AutoStartupSwitchText {
+            get {
+                return ResourceManager.GetString("AutoStartupSwitchText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enable auto update.
+        /// </summary>
+        internal static string AutoUpdateSwitchText {
+            get {
+                return ResourceManager.GetString("AutoUpdateSwitchText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Baud Rate:.
+        /// </summary>
+        internal static string BaudRateText {
+            get {
+                return ResourceManager.GetString("BaudRateText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cancel.
+        /// </summary>
+        internal static string CancelButtonTooltip {
+            get {
+                return ResourceManager.GetString("CancelButtonTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Check for updates.
+        /// </summary>
+        internal static string CheckForUpdatesTooltip {
+            get {
+                return ResourceManager.GetString("CheckForUpdatesTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Close application.
+        /// </summary>
+        internal static string CloseAppFromTrayButtonText {
+            get {
+                return ResourceManager.GetString("CloseAppFromTrayButtonText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Close HSMonitor.
+        /// </summary>
+        internal static string CloseButtonTooltip {
+            get {
+                return ResourceManager.GetString("CloseButtonTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to CPU icon:.
+        /// </summary>
+        internal static string CustomCPUIconText {
+            get {
+                return ResourceManager.GetString("CustomCPUIconText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Custom CPU name:.
+        /// </summary>
+        internal static string CustomCPUNameText {
+            get {
+                return ResourceManager.GetString("CustomCPUNameText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to GPU icon:.
+        /// </summary>
+        internal static string CustomGPUIconText {
+            get {
+                return ResourceManager.GetString("CustomGPUIconText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Custom GPU name:.
+        /// </summary>
+        internal static string CustomGPUNameText {
+            get {
+                return ResourceManager.GetString("CustomGPUNameText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to RAM icon:.
+        /// </summary>
+        internal static string CustomRAMIconText {
+            get {
+                return ResourceManager.GetString("CustomRAMIconText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Default CPU Frequency(MHz):.
+        /// </summary>
+        internal static string DefaultCPUFreqText {
+            get {
+                return ResourceManager.GetString("DefaultCPUFreqText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Default GPU Frequency(MHz):.
+        /// </summary>
+        internal static string DefaultGPUFreqText {
+            get {
+                return ResourceManager.GetString("DefaultGPUFreqText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Display brightness:.
+        /// </summary>
+        internal static string DisplayBrightnessText {
+            get {
+                return ResourceManager.GetString("DisplayBrightnessText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Start hidden.
+        /// </summary>
+        internal static string HiddentStartSwitchText {
+            get {
+                return ResourceManager.GetString("HiddentStartSwitchText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Project URL on GitHub:.
+        /// </summary>
+        internal static string ProjectURLText {
+            get {
+                return ResourceManager.GetString("ProjectURLText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reset all settings to their defaults.
+        /// </summary>
+        internal static string ResetButtonTooltip {
+            get {
+                return ResourceManager.GetString("ResetButtonTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Run as administrator.
+        /// </summary>
+        internal static string RunAsAdminSwitchText {
+            get {
+                return ResourceManager.GetString("RunAsAdminSwitchText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save settings.
+        /// </summary>
+        internal static string SaveButtonTooltip {
+            get {
+                return ResourceManager.GetString("SaveButtonTooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Select CPU:.
+        /// </summary>
+        internal static string SelectCPUText {
+            get {
+                return ResourceManager.GetString("SelectCPUText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Select GPU:.
+        /// </summary>
+        internal static string SelectGPUText {
+            get {
+                return ResourceManager.GetString("SelectGPUText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Serial port:.
+        /// </summary>
+        internal static string SerialPortText {
+            get {
+                return ResourceManager.GetString("SerialPortText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Settings.
+        /// </summary>
+        internal static string SettingsButtonContent {
+            get {
+                return ResourceManager.GetString("SettingsButtonContent", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Show window.
+        /// </summary>
+        internal static string ShowWindowFromTrayButtonText {
+            get {
+                return ResourceManager.GetString("ShowWindowFromTrayButtonText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to TX Interval(ms):.
+        /// </summary>
+        internal static string TXIntervalText {
+            get {
+                return ResourceManager.GetString("TXIntervalText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Update Download Progress.
+        /// </summary>
+        internal static string UpdateDownloadProgressTooltip {
+            get {
+                return ResourceManager.GetString("UpdateDownloadProgressTooltip", resourceCulture);
             }
         }
     }
