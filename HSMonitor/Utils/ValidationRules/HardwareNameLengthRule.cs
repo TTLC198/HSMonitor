6bd9b5a35b9ad94ba33f6ebef1c2222b6595f6d0
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-09-09 12:31:08 +0300

    Added Min field to check custom input of processor or video card name

--- HSMonitor/Utils/ValidationRules/HardwareNameLengthRule.cs
+++ HSMonitor/Utils/ValidationRules/HardwareNameLengthRule.cs
@@ -7,6 +7,8 @@ namespace HSMonitor.Utils.ValidationRules;
 public class HardwareNameLengthRule: ValidationRule
 {
     public int Max { get; set; }
+    
+    public int Min { get; set; }
 
     public override ValidationResult Validate(object value, CultureInfo cultureInfo)
     {
@@ -15,6 +17,10 @@ public class HardwareNameLengthRule: ValidationRule
             if (((string)value).Length > Max)
                 return new ValidationResult(false,
                     $"Please enter a string less than {Max}.");
+            
+            if (((string)value).Length < Min)
+                return new ValidationResult(false,
+                    $"Please enter a string more than {Min}.");
         }
         catch (Exception e)
         {
