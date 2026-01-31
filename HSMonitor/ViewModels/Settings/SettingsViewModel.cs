b548c2682d1c440ef3b15c49ab4178170beff25b
ttlc198 <ttlc198@imbalanced.tech>
2026-01-31 19:47:43 +0300

    Переделан внешний вид окна настроек

--- HSMonitor/ViewModels/Settings/SettingsViewModel.cs
+++ HSMonitor/ViewModels/Settings/SettingsViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿﻿using System.Collections.Generic;
 using System.Linq;
 using HSMonitor.Services;
 using HSMonitor.ViewModels.Framework;
@@ -30,11 +30,73 @@ public class SettingsViewModel : DialogScreen<bool>, IOpenInOwnWindowDialog
         // Deactivate previously selected tab
         if (ActiveTab is not null)
             ActiveTab.IsSelected = false;
-        
+
         ActiveTab = settingsTab;
         settingsTab.IsSelected = true;
     }
-    
+
+    public void NextTab()
+    {
+        if (Tabs.Count == 0)
+            return;
+
+        if (ActiveTab is null)
+        {
+            ActivateTab(Tabs[0]);
+            return;
+        }
+
+        var currentIndex = -1;
+        for (var i = 0; i < Tabs.Count; i++)
+        {
+            if (ReferenceEquals(Tabs[i], ActiveTab))
+            {
+                currentIndex = i;
+                break;
+            }
+        }
+
+        if (currentIndex < 0)
+        {
+            ActivateTab(Tabs[0]);
+            return;
+        }
+
+        var nextIndex = (currentIndex + 1) % Tabs.Count;
+        ActivateTab(Tabs[nextIndex]);
+    }
+
+    public void PrevTab()
+    {
+        if (Tabs.Count == 0)
+            return;
+
+        if (ActiveTab is null)
+        {
+            ActivateTab(Tabs[0]);
+            return;
+        }
+
+        var currentIndex = -1;
+        for (var i = 0; i < Tabs.Count; i++)
+        {
+            if (ReferenceEquals(Tabs[i], ActiveTab))
+            {
+                currentIndex = i;
+                break;
+            }
+        }
+
+        if (currentIndex < 0)
+        {
+            ActivateTab(Tabs[0]);
+            return;
+        }
+
+        var prevIndex = (currentIndex - 1 + Tabs.Count) % Tabs.Count;
+        ActivateTab(Tabs[prevIndex]);
+    }
+
     public void ActivateTabByType<T>() where T : ISettingsTabViewModel
     {
         var tab = Tabs.OfType<T>().FirstOrDefault();
@@ -42,7 +104,7 @@ public class SettingsViewModel : DialogScreen<bool>, IOpenInOwnWindowDialog
             ActivateTab(tab);
     }
 
-    public async void Reset() => 
+    public async void Reset() =>
         await _settingsService.Reset();
 
     public async void Save()
@@ -56,6 +118,7 @@ public class SettingsViewModel : DialogScreen<bool>, IOpenInOwnWindowDialog
         await _settingsService.LoadAsync();
         Close(false);
     }
+
     public string Title => "Настройки";
     public double Width => 500;
     public double MinWidth => 360;
