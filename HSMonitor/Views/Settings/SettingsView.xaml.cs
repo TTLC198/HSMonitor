555dc16d8fb3d923470c38359aa4b69ff10966cf
ttlc198 <ttlc198@imbalanced.tech>
2026-02-01 12:42:32 +0300

    Подредактировал внешний вид

--- HSMonitor/Views/Settings/SettingsView.xaml.cs
+++ HSMonitor/Views/Settings/SettingsView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace HSMonitor.Views.Settings;
 
@@ -7,5 +10,29 @@ public partial class SettingsView : UserControl
     public SettingsView()
     {
         InitializeComponent();
+    }
+    
+    private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        // Ищем ScrollViewer в визуальном дереве и скроллим его
+        if (sender is not DependencyObject d) return;
+
+        var sv = FindChild<ScrollViewer>(d);
+        if (sv == null) return;
+
+        sv.ScrollToVerticalOffset(sv.VerticalOffset - e.Delta);
+        e.Handled = true;
+    }
+
+    private static T? FindChild<T>(DependencyObject root) where T : DependencyObject
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(root); i++)
+        {
+            var child = VisualTreeHelper.GetChild(root, i);
+            if (child is T t) return t;
+            var res = FindChild<T>(child);
+            if (res != null) return res;
+        }
+        return null;
     }
 }
