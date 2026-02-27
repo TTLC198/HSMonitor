7b3b2ccecb9fafb42214370c17202c4ad6b9f240
TTLC198 <yorehacked@gmail.com>
2026-02-27 16:59:08 +0300

    Начало работы над установкой PawnIO

---
+++ HSMonitor/Utils/InstallerService.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using HSMonitor.Views;
+
+namespace HSMonitor.Utils;
+
+public static class InstallerService
+{
+    static void InstallPawnIo()
+    {
+        var path = ExtractPawnIo();
+        if (string.IsNullOrEmpty(path)) return;
+        var process = Process.Start(new ProcessStartInfo(path, "-install"));
+        process?.WaitForExit();
+
+        File.Delete(path);
+    }
+    
+    static string ExtractPawnIo()
+    {
+        var destination = Path.Combine(Directory.GetCurrentDirectory(), "PawnIO_setup.exe");
+
+        try
+        {
+            using var resourceStream = typeof(MainWindowView).Assembly.GetManifestResourceStream("HSMonitor.Resources.PawnIO_setup.exe");
+            using FileStream fileStream = new(destination, FileMode.Create, FileAccess.Write);
+            resourceStream?.CopyTo(fileStream);
+
+            return destination;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
